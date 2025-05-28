613de0294af0a32ce84ae2d316a8d8cd3011038a
Juan Carlos Ramos Cruz <jcramosc@desarrollo-ultrasist.com.mx>
2025-05-27 19:20:56 -0600

    Tramite Finalizado 130115, 130113, 130111,130116, 130107, 130104, 270301 en proceso 270101.

---
+++ automations/src/test/java/Economia/Tramite130113/MainPage130113Test.java
@@ -0,0 +1,179 @@
+package Economia.Tramite130113;
+
+import DBYFOLIO.ObtenerFolio;
+import Firmas.LoginFirmSoli;
+import Firmas.TramitesFirmasLG;
+import Metodos.Metodos;
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class MainPage130113Test {
+    private static final Logger log = LoggerFactory.getLogger(MainPage130113Test.class);
+    MainPage130113 mainPage130113 = new MainPage130113();
+    LoginFirmSoli loginFirmSoli = new LoginFirmSoli();
+    Metodos metodos = new Metodos();
+
+    ObtenerFolio obtenerFolio = new ObtenerFolio();
+    //VARIABLES
+    String FunRFC = "MAVL621207C95";
+    String SoliRFC = "AAL0409235E6";
+
+    TramitesFirmasLG tramite130113  = new TramitesFirmasLG(
+            "C:\\VucemAuto\\automations\\src\\test\\resources\\CredSoli\\aal0409235e6.cer",
+            "C:\\VucemAuto\\automations\\src\\test\\resources\\CredSoli\\AAL0409235E6_1012231310.key"
+    );
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        iniDriver();
+    }
+
+    public static void iniDriver(){
+        Configuration.browser = Browsers.CHROME;
+        open();
+        getWebDriver().manage().window().maximize();
+        getWebDriver().manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void Proceso130113() {
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////-
+        // Solicitar el número de repeticiones al usuario a través de un JOptionPane con opción de Cancelar
+        String repeticionesStr = JOptionPane.showInputDialog(null, "Ingrese el número de repeticiones:", "Repeticiones", JOptionPane.QUESTION_MESSAGE);
+
+        // Si el usuario cancela o cierra el cuadro de diálogo, repeticionesStr será null
+        if (repeticionesStr == null) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada por el usuario.");
+            return; // Termina el metodo si se selecciona cancelar
+        }
+
+        // Convertir el valor ingresado a entero, manejando posibles excepciones si el usuario no ingresa un número válido
+        int repeticiones;
+        try {
+            repeticiones = Integer.parseInt(repeticionesStr);
+        } catch (NumberFormatException e) {
+            // Si el valor ingresado no es un número válido, asigna un valor por defecto (por ejemplo, 1)
+            repeticiones = 1;
+            JOptionPane.showMessageDialog(null, "Valor no válido, se utilizará 1 repetición por defecto.");
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////-
+
+        //Crear checkboxes para seleccionar los métodos
+        JCheckBox dictamenCheckBox = new JCheckBox("ProcesoDictamen130113");
+        JCheckBox autorizacionCheckBox = new JCheckBox("ProcesoAutorizacion130113");
+        JCheckBox confirmacionCheckBox = new JCheckBox("ProcesoConfirmarNotificaciónResolucion130113");
+
+        Object[] params = {"Seleccione los métodos a ejecutar:", dictamenCheckBox, autorizacionCheckBox, confirmacionCheckBox};
+        int option = JOptionPane.showConfirmDialog(null, params, "Opciones de Métodos", JOptionPane.OK_CANCEL_OPTION);
+
+        // Si el usuario selecciona Cancelar, termina el metodo
+        if (option != JOptionPane.OK_OPTION) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada por el usuario.");
+            return;
+        }
+
+        // Recopilar los métodos seleccionados
+        List<String> selectedMethods = new ArrayList<>();
+        if (dictamenCheckBox.isSelected()) selectedMethods.add("ProcesoDictamen130113");
+        if (autorizacionCheckBox.isSelected()) selectedMethods.add("ProcesoAutorizacion130113");
+        if (confirmacionCheckBox.isSelected()) selectedMethods.add("ProcesoConfirmarNotificaciónResolucion130113");
+
+        // Ejecutar el proceso con las repeticiones y los métodos seleccionados
+        ejecutarProcesoNRunnable(() -> {
+//            // Ingreso y selección de trámite
+            loginFirmSoli.login(tramite130113);
+            mainPage130113.selecRol.sendKeys("Persona Moral");
+            mainPage130113.btnacep.click();
+            mainPage130113.Tramites.sendKeys("Solicitudes nuevas");
+            mainPage130113.SoliNew.click();
+            mainPage130113.economia.click();
+            mainPage130113.direccionGeneralNormas.click();
+            mainPage130113.importacion.click();
+            mainPage130113.importacionEquiAntiContPart.click();
+            try {
+                Thread.sleep(2000); // Pausa de 3 segundos
+                // Hacer scroll hasta el elemento
+                mainPage130113.Scrol.scrollIntoView(true);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mainPage130113.labelDatosSolicitud.click();
+            mainPage130113.inputRegimen.sendKeys("Definitivos");
+            mainPage130113.inputClasificacion.sendKeys(" De importación");
+            mainPage130113.inputDescripcion.sendKeys("PRUEBAS");
+            mainPage130113.inputFraccionArancelaria.sendKeys("98060002");
+            mainPage130113.inputCantidad.sendKeys("100");
+            mainPage130113.inputValorFactura.sendKeys("100");
+            mainPage130113.inputUMT.sendKeys("Gramo");
+            mainPage130113.inputCantidadPartida.sendKeys("100");
+            mainPage130113.inputValorPartida.sendKeys("100");
+            mainPage130113.inputFraccionArancelariaTIGIE.sendKeys("30019099");
+            mainPage130113.getInputFraccionArancelariaTIGIE2.sendKeys("30019099");
+            mainPage130113.inputDescripcionPartida.sendKeys("PRUEBAS");
+            mainPage130113.btnAgregarPartida.click();
+            mainPage130113.inputBloquePaisDestino.click();sleep(1000);
+            mainPage130113.inputPaisDedstino.sendKeys("ESTADOS UNIDOS DE AMERICA");sleep(1000);
+            mainPage130113.btnAgregarBloque.click();sleep(1000);
+            mainPage130113.textareaUsoEspecifico.sendKeys("PRUEBA");
+            mainPage130113.textareaJustificacion.sendKeys("PRUEBA");
+            mainPage130113.textareaObservaciones.sendKeys("PRUEBAS");
+            mainPage130113.inputEntidad.sendKeys("SINALOA");
+            mainPage130113.inputRepresentacionFederal.sendKeys("CULIACAN");
+            mainPage130113.inputGuardarSolicitud.click();
+            mainPage130113.inputGuarda.click();sleep(1000);
+            metodos.cargarDocumentos();
+            mainPage130113.btnAnexar.click();sleep(20000);
+            mainPage130113.btnCerrar.click();
+            mainPage130113.inputSiguiente.click();
+            //FIRMAR SOLICITUD
+            loginFirmSoli.firma(tramite130113);
+            String folioText = mainPage130113.folio.getText();
+            String folioNumber = obtenerFolio.obtenerFolio(folioText);
+        }, repeticiones);
+    }
+
+    //Metodo que ejecuta n veces una clase que implementa Runnable
+    public void ejecutarProcesoNRunnable(Runnable proceso, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ejecución del Proceso: " + (i + 1));
+            open("https://wwwqa.ventanillaunica.gob.mx/ventanilla-HA/authentication.action?showLogin=%22;");
+            proceso.run();  // Ejecuta el proceso de la clase
+        }
+    }
+
+    public void presionarEscYSpace() {
+        // Utilizamos WebDriverRunner para obtener el WebDriver actual
+        new Actions(WebDriverRunner.getWebDriver())
+                .sendKeys(Keys.ESCAPE) // Presiona la tecla Esc
+                .sendKeys(Keys.SPACE)   // Luego presiona la barra espaciadora (Space)
+                .perform();             // Ejecuta las acciones
+    }
+}
---
+++ automations/src/test/java/Economia/Tramite130115/MainPage130115Test.java
@@ -0,0 +1,179 @@
+package Economia.Tramite130115;
+
+import DBYFOLIO.ObtenerFolio;
+import Firmas.LoginFirmSoli;
+import Firmas.TramitesFirmasLG;
+import Metodos.Metodos;
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class MainPage130115Test {
+    private static final Logger log = LoggerFactory.getLogger(MainPage130115Test.class);
+    MainPage130115 mainPage130115 = new MainPage130115();
+    LoginFirmSoli loginFirmSoli = new LoginFirmSoli();
+    Metodos metodos = new Metodos();
+
+    ObtenerFolio obtenerFolio = new ObtenerFolio();
+    //VARIABLES
+    String FunRFC = "MAVL621207C95";
+    String SoliRFC = "AAL0409235E6";
+
+    TramitesFirmasLG tramite130115  = new TramitesFirmasLG(
+            "C:\\VucemAuto\\automations\\src\\test\\resources\\CredSoli\\aal0409235e6.cer",
+            "C:\\VucemAuto\\automations\\src\\test\\resources\\CredSoli\\AAL0409235E6_1012231310.key"
+    );
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        iniDriver();
+    }
+
+    public static void iniDriver(){
+        Configuration.browser = Browsers.CHROME;
+        open();
+        getWebDriver().manage().window().maximize();
+        getWebDriver().manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void Proceso130115() {
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////-
+        // Solicitar el número de repeticiones al usuario a través de un JOptionPane con opción de Cancelar
+        String repeticionesStr = JOptionPane.showInputDialog(null, "Ingrese el número de repeticiones:", "Repeticiones", JOptionPane.QUESTION_MESSAGE);
+
+        // Si el usuario cancela o cierra el cuadro de diálogo, repeticionesStr será null
+        if (repeticionesStr == null) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada por el usuario.");
+            return; // Termina el metodo si se selecciona cancelar
+        }
+
+        // Convertir el valor ingresado a entero, manejando posibles excepciones si el usuario no ingresa un número válido
+        int repeticiones;
+        try {
+            repeticiones = Integer.parseInt(repeticionesStr);
+        } catch (NumberFormatException e) {
+            // Si el valor ingresado no es un número válido, asigna un valor por defecto (por ejemplo, 1)
+            repeticiones = 1;
+            JOptionPane.showMessageDialog(null, "Valor no válido, se utilizará 1 repetición por defecto.");
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////-
+
+        //Crear checkboxes para seleccionar los métodos
+        JCheckBox dictamenCheckBox = new JCheckBox("ProcesoDictamen130115");
+        JCheckBox autorizacionCheckBox = new JCheckBox("ProcesoAutorizacion130115");
+        JCheckBox confirmacionCheckBox = new JCheckBox("ProcesoConfirmarNotificaciónResolucion130115");
+
+        Object[] params = {"Seleccione los métodos a ejecutar:", dictamenCheckBox, autorizacionCheckBox, confirmacionCheckBox};
+        int option = JOptionPane.showConfirmDialog(null, params, "Opciones de Métodos", JOptionPane.OK_CANCEL_OPTION);
+
+        // Si el usuario selecciona Cancelar, termina el metodo
+        if (option != JOptionPane.OK_OPTION) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada por el usuario.");
+            return;
+        }
+
+        // Recopilar los métodos seleccionados
+        List<String> selectedMethods = new ArrayList<>();
+        if (dictamenCheckBox.isSelected()) selectedMethods.add("ProcesoDictamen130115");
+        if (autorizacionCheckBox.isSelected()) selectedMethods.add("ProcesoAutorizacion130115");
+        if (confirmacionCheckBox.isSelected()) selectedMethods.add("ProcesoConfirmarNotificaciónResolucion130115");
+
+        // Ejecutar el proceso con las repeticiones y los métodos seleccionados
+        ejecutarProcesoNRunnable(() -> {
+//            // Ingreso y selección de trámite
+            loginFirmSoli.login(tramite130115);
+            mainPage130115.selecRol.sendKeys("Persona Moral");
+            mainPage130115.btnacep.click();
+            mainPage130115.Tramites.sendKeys("Solicitudes nuevas");
+            mainPage130115.SoliNew.click();
+            mainPage130115.economia.click();
+            mainPage130115.direccionGeneralNormas.click();
+            mainPage130115.importacion.click();
+            mainPage130115.importacionVehiConNuevMomIngDepoFis.click();
+            try {
+                Thread.sleep(2000); // Pausa de 3 segundos
+                // Hacer scroll hasta el elemento
+                mainPage130115.Scrol.scrollIntoView(true);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mainPage130115.labelDatosSolicitud.click();
+            mainPage130115.inputRegimen.sendKeys("Definitivos");
+            mainPage130115.inputClasificacion.sendKeys(" De importación");
+            mainPage130115.inputProducto.click();
+            mainPage130115.inputDescripcion.sendKeys("PRUEBAS");
+            mainPage130115.inputFraccionArancelaria.sendKeys("87012101");
+            mainPage130115.inputCantidad.sendKeys("100");
+            mainPage130115.inputValorFactura.sendKeys("100");
+            mainPage130115.inputUMT.sendKeys("Gramo");
+            mainPage130115.inputCantidadPartida.sendKeys("100");
+            mainPage130115.inputValorPartida.sendKeys("100");
+            mainPage130115.inputDescripcionPartida.sendKeys("PRUEBAS");
+            mainPage130115.btnAgregarPartida.click();
+            mainPage130115.inputBloquePaisDestino.click();sleep(1000);
+            mainPage130115.inputPaisDedstino.sendKeys("ESTADOS UNIDOS DE AMERICA");sleep(1000);
+            mainPage130115.btnAgregarBloque.click();sleep(1000);
+            mainPage130115.textareaUsoEspecifico.sendKeys("PRUEBA");
+            mainPage130115.textareaJustificacion.sendKeys("PRUEBA");
+            mainPage130115.textareaObservaciones.sendKeys("PRUEBAS");
+            mainPage130115.inputEntidad.sendKeys("SINALOA");
+            mainPage130115.inputRepresentacionFederal.sendKeys("CULIACAN");
+            mainPage130115.inputGuardarSolicitud.click();
+            mainPage130115.inputGuarda.click();sleep(1000);
+            metodos.cargarDocumentos();
+            mainPage130115.btnAnexar.click();sleep(2000);
+            mainPage130115.btnCerrar.click();
+            mainPage130115.inputSiguiente.click();
+            //FIRMAR SOLICITUD
+            loginFirmSoli.firma(tramite130115);
+            String folioText = mainPage130115.folio.getText();
+            String folioNumber = obtenerFolio.obtenerFolio(folioText);
+        }, repeticiones);
+    }
+
+    //Metodo que ejecuta n veces una clase que implementa Runnable
+    public void ejecutarProcesoNRunnable(Runnable proceso, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ejecución del Proceso: " + (i + 1));
+            open("https://wwwqa.ventanillaunica.gob.mx/ventanilla-HA/authentication.action?showLogin=%22;");
+            proceso.run();  // Ejecuta el proceso de la clase
+        }
+    }
+
+    public void presionarEscYSpace() {
+        // Utilizamos WebDriverRunner para obtener el WebDriver actual
+        new Actions(WebDriverRunner.getWebDriver())
+                .sendKeys(Keys.ESCAPE) // Presiona la tecla Esc
+                .sendKeys(Keys.SPACE)   // Luego presiona la barra espaciadora (Space)
+                .perform();             // Ejecuta las acciones
+    }
+
+}
