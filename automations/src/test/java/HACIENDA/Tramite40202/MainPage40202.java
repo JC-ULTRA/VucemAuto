7a58a2c0dece228d1af4452cbf8dbd008c440f4c
JnoemUltra <jnoem@ultrasist.com.mx>
2025-09-26 13:30:57 -0600

    40202 listo

---
+++ automations/src/test/java/HACIENDA/Tramite40202/MainPage40202.java
@@ -0,0 +1,24 @@
+package HACIENDA.Tramite40202;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage40202 {
+    public SelenideElement cerS = $x("//*[@id='certificate']");
+    public SelenideElement keyS = $x("//*[@id='privateKey']");
+    public SelenideElement pass = $x("//*[@id='privateKeyPassword']");
+    public SelenideElement btnIngresFs = $x("//button[@onclick='ejecutarProcesoFirma(1)']");
+    public SelenideElement btnFirmar = $x("//*[@id='btnSubmit']");
+    public SelenideElement Scrol = Selenide.$x("//*[@id=\"logoHeader\"]");
+    public SelenideElement folio = $x("/html/body/main/div/div[4]/div/div[4]/div/div/ul/li");
+    public SelenideElement selecRol = $x("//*[@id='claveRol']");
+    public SelenideElement btnacep = $x("//button[@name='ingresar']");
+    public SelenideElement Tramites = $x("//*[@id='Trámites']");
+    public SelenideElement SoliNew = $x("/html/body/main/div/div[2]/div[1]/div[1]/div/nav/div/div[2]/ul/li[5]/ul/li[3]/a");
+    public SelenideElement SEDENA = $x("//*[@id=\"workingArea\"]/form/a[5]/img");
+    public SelenideElement linkCerLicPer = $x("/html/body/main/div/div[3]/div/div[6]/ul/li/a");
+    public SelenideElement linkPermisoAduanalSEDENA = $x("/html/body/main/div/div[3]/div/div[6]/ul/li/ul/li[1]/a");
+    public SelenideElement linkPermisoOrdExpArtPirotecnicos = $x("/html/body/main/div/div[3]/div/div[6]/ul/li/ul/li[1]/ul/li[16]/a");
+}
