8523425c2adbd38cbd860765c51daadfe2eff0e0
Juan Carlos Ramos Cruz <jcramosc@desarrollo-ultrasist.com.mx>
2025-10-06 18:41:36 -0600

    Tramite Finalizado: 290301

--- automations/src/test/java/AMECAFE/Tramite290301/MainPage290301.java
+++ automations/src/test/java/AMECAFE/Tramite290301/MainPage290301.java
@@ -14,6 +14,7 @@ public class MainPage290301 {
     public SelenideElement selecRol = $x("//*[@id='claveRol']");
     public SelenideElement btnacep = $x("//button[@name='ingresar']");
     public SelenideElement Tramites = $x("//*[@id='Trámites']");
+    public SelenideElement FolioBusqueda = $x("//*[@id=\"idFolio\"]");
     public SelenideElement SoliSub = $x("/html/body/main/div/div[2]/div[1]/div[1]/div/nav/div/div[2]/ul/li[5]/ul/li[4]/a");
     public SelenideElement inputIdFolio = $x("//*[@id=\"idFolio\"]");
     public SelenideElement inputBuscarButton = $x("//*[@id=\"buscarButton\"]");
@@ -22,11 +23,7 @@ public class MainPage290301 {
     public SelenideElement Scrol = Selenide.$x("//*[@id=\"logoHeader\"]");
     //DATOS SOLICITUD
     public SelenideElement labelDatosSolicitud = $x("//*[@id=\"ui-id-2\"]/label");
-
-
-
-
-
+    public SelenideElement inputJustificacion = $x("//*[@id=\"justificacionId\"]");
     public SelenideElement inputGuardarSolicitud = $x("//*[@id=\"guardarSolicitud\"]");
     public SelenideElement inputGuarda = $x("//*[@id=\"workingArea\"]/form/div[4]/div/div/input[2]");
     public SelenideElement inputAdjuntar = $x("/html/body/main/div/div[4]/div/form[1]/table/tbody/tr[5]/td[2]/div/input[2]");
@@ -37,4 +34,17 @@ public class MainPage290301 {
     public SelenideElement btnCerrar = $x("/html/body/main/div/div[4]/div[2]/form/div[3]/div/input");
     public SelenideElement inputSiguiente = $x("/html/body/main/div/div[4]/div[1]/form[2]/div[2]/div/input[2]");
     public SelenideElement folio = $x("/html/body/main/div/div[4]/div/div[4]/div/div/ul/li");
+    //Requerimiento
+    public SelenideElement iniciofun = $x("/html/body/div[1]/div[3]/div[1]/table/tbody/tr[2]/td/a/img");
+    public SelenideElement numfolio = $x("//*[@id=\"idNumFolio\"]");
+    public SelenideElement btnBuscarFolioR = $x("//*[@id=\"buscarTareasFuncionario\"]");
+    public SelenideElement inputDictamenAceptado = $x("/html/body/div[1]/div[3]/div[3]/div[1]/form/table/tbody/tr[2]/td[2]/span/input");
+    public SelenideElement justificacionRequerimiento = $x("//*[@id=\"valueTA\"]");
+    public SelenideElement inputPendiente = $x("//*[@id=\"pendienteActivar\"]");
+    //Autorización
+    public SelenideElement btnFirmarAutorizacion = $x("/html/body/div[1]/div[3]/div[3]/div/form[1]/div[2]/input[2]");
+    //Firmar
+    public SelenideElement SelecRol = Selenide.$x("//*[@id='claveRol']");
+    public SelenideElement Btnacep = Selenide.$x("//button[@name='ingresar']");
+    public SelenideElement inicioFolio = $x("//*[@id=\"idNumFolio\"]");
 }
